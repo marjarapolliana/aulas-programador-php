7906b0fc482c443b7d990a8cc20d869869319547
Marjara Polliana Araujo da Silva <marjaracx10@gmail.com>
2026-08-13 21:41:11 -0300

    adicionar pratica com variaveis

---
+++ 01-introducao-ao-php/02-variaveis-pratica/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$nome = "Márjara Polliana";
+$anoDeNascimento = 1988;
+$curso = "Programador Web";
+
+$altura = 1.65;
+$peso = 57;
+
+$temTatuagem = false;
+
+echo "Meu nome é {$nome} e eu faço o curso de {$curso}";
+
+var_dump($altura, $peso);
+var_dump($temTatuagem);
+
+
+// Exercicío Prático
+
+$nomeDoFilme = "VINGADORES - GUERRA INFINITA";
+$genero = "Ficção Científica";
+$anoDeLancamento = 2018;
+$sinopse = "É um filme de super herois da marvel";
+
+echo "<h2>{$nomeDoFilme}</h2>";
+echo "<h3>{$genero}</h3";
+echo "<p>{$anoDeLancamento}</p>";
+echo "<p>{$sinopse}<p>";
+
+
+
+
